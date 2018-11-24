eea0efaa5bfc99fadff59cbfcfe48a4878729b68
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-24 15:41:19 +0100

    Renamed TheClient to TheClientReceiver

---
+++ TheClient/ClientReceiver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using IRemote;
+using System.Collections;
+
+namespace TheClientReceiver {
+	class ClientReceiver {
+		
+		static void Main(string[] args) {
+			try{
+				TcpChannel chl = new TcpChannel();
+				ChannelServices.RegisterChannel(chl, false);
+				IMailBox obj = (IMailBox)Activator.GetObject(typeof(IMailBox),"tcp://localhost:1234/mailBoxObj");
+
+
+				if (obj == null ){
+					Console.WriteLine("[!] Object not resolved. " );
+				}else {
+					Console.WriteLine( "[+] Object recieved." );
+
+
+					ArrayList msgs = obj.receiveMsg();
+
+					foreach (Message msg in msgs){
+						Console.WriteLine("The sender: {0}\nThe reciever: {1}\nThe content: {2}\n\n", msg.sender, msg.receiver, msg.content);
+					}
+				}
+			}
+			catch (Exception e ) {
+				Console.WriteLine( "There was an error in the ClientSender : {0}", e.Message );
+			}
+		} 
+	} 
+}
