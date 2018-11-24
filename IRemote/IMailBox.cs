76ba4208f28c82fee1e40d19dcfd27938dae7199
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-24 13:32:11 +0100

    Added the IRemote library

---
+++ IRemote/IMailBox.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace IRemote
+{
+	public interface IMailBox{
+		void sendMsg(Message msg);
+		Message[] receiveMsg() ;
+	}
+}
+
---
+++ IRemote/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IRemote
+{
+	public class Message
+	{
+		private String sender { set; get; }
+		private String receiver { set; get; }
+		private String content { set; get; }
+
+		public Message (String sender, String receiver, String content)
+		{
+			this.sender = sender;
+			this.receiver = receiver;
+			this.content = content;
+		}
+
+	}
+}
+
