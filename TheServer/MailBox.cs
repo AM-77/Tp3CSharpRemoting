5b031f61410d4fd50e12ab876730a0d806327339
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-24 13:53:18 +0100

    Added the Server project.

---
+++ TheServer/MailBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace TheServer
+{
+	public class MailBox : MarshalByRefObject, IRemote.IMailBox
+	{
+		
+		private ArrayList msgs = new ArrayList();
+
+		public MailBox ()
+		{
+		}
+
+		void sendMsg(Message msg){
+			msgs.Add (msg);
+		}
+
+		Message[] receiveMsg(){
+			return msgs;
+		}
+	}
+}
+
---
+++ TheServer/ServerMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using IRemote;
+
+namespace TheServer
+{
+	class ServerMain
+	{
+		public static void Main (string[] args)
+		{
+			try{
+
+				TcpChannel tcpChannel = new TcpChannel(1995);
+				ChannelServices.RegisterChannel(tcpChannel, false);
+				RemotingConfiguration.RegisterActivatedServiceType(typeof(MailBox), "mailBoxObj",WellKnownObjectMode.Singleton);
+
+				Console.Write("[+] The server is running ...");
+
+			}catch(Exception e){
+				Console.Write ("[!] There was an error in the serverMain : {0}", e.Message);
+			}
+		}
+	}
+}
