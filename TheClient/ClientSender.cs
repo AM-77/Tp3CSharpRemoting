1515f611cfae9f39ea7fc112f930526d0ec19022
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-24 14:45:57 +0100

    Added The Client project.

---
+++ TheClient/ClientSender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using IRemote;
+
+namespace TheClient {
+	class ClientSender {
+		static void Main(string[] args) {
+			try{
+				TcpChannel chl = new TcpChannel();
+				ChannelServices.RegisterChannel(chl, false);
+				Console.WriteLine( "Client: Canal enregistré" );
+				IMailBox obj = (IMailBox)Activator.GetObject(typeof(IMailBox),"tcp://localhost:1234/mailBoxObj");
+
+
+				if (obj == null )
+
+					Console.WriteLine("Problème de SERVEUR... " );
+				else {
+					Console.WriteLine( "Reference acquise de l'objet Singleton" );
+					obj.receiveMsg();
+					Console.ReadLine();
+				}
+			}
+			catch (Exception e ) {
+				Console.WriteLine( "There was an error in the ClientSender : {0}", e.Message );
+			}
+		} 
+	} 
+}
