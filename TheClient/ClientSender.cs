e04f23eccecc85894199fa3f58310743d8fd7787
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-24 15:04:43 +0100

    Fnished the Clinet.

--- TheClient/ClientSender.cs
+++ TheClient/ClientSender.cs
@@ -3,24 +3,27 @@ using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
 using IRemote;
+using System.Collections;
 
 namespace TheClient {
 	class ClientSender {
+		
 		static void Main(string[] args) {
 			try{
 				TcpChannel chl = new TcpChannel();
 				ChannelServices.RegisterChannel(chl, false);
-				Console.WriteLine( "Client: Canal enregistré" );
 				IMailBox obj = (IMailBox)Activator.GetObject(typeof(IMailBox),"tcp://localhost:1234/mailBoxObj");
 
 
-				if (obj == null )
+				if (obj == null ){
+					Console.WriteLine("[!] Object not resolved. " );
+				}else {
+					Console.WriteLine( "[+] Object recieved." );
+					ArrayList msgs = obj.receiveMsg();
 
-					Console.WriteLine("Problème de SERVEUR... " );
-				else {
-					Console.WriteLine( "Reference acquise de l'objet Singleton" );
-					obj.receiveMsg();
-					Console.ReadLine();
+					foreach (Message msg in msgs){
+						Console.WriteLine("The sender: {0}\nThe reciever: {1}\nThe content: {2}\n\n", msg.sender, msg.receiver, msg.content);
+					}
 				}
 			}
 			catch (Exception e ) {
