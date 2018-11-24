ab10faff82d6014cd6e141f789fb495f1806e5d7
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-24 15:43:05 +0100

    Added the sender client receiver

---
+++ TheClientSender/ClientSender.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using IRemote;
+using System.Collections;
+
+namespace TheClientSender {
+	class ClientSender {
+
+		static void Main(string[] args) {
+			try{
+				TcpChannel chl = new TcpChannel();
+				ChannelServices.RegisterChannel(chl, false);
+				IMailBox mailBox = (IMailBox)Activator.GetObject(typeof(IMailBox),"tcp://localhost:1234/mailBoxObj");
+
+
+				if (mailBox == null ){
+					Console.WriteLine("[!] Object not resolved. " );
+				}else {
+					Console.WriteLine( "[+] Object recieved.\n" );
+					Boolean sending = true;
+					Console.Write( "Sender Name: " );
+					String sender = Console.ReadLine();
+
+					while(sending){
+						Console.Write( "Receiver Name: " );
+						String receiver = Console.ReadLine();
+						Console.Write( "The message: " );
+						String msg = Console.ReadLine();
+
+						mailBox.sendMsg(new Message(sender, receiver, msg));
+
+						Console.Write( "[+] Message sent.\nSend an other message? y/n : " );
+						String resend = Console.ReadLine();
+						while(true){
+							if( resend != "y" && resend != "n" ){
+								Console.Write( "[!] Invalid input try with y/n : " );
+								resend = Console.ReadLine();
+							}else{
+								if(resend == "n"){
+									sending = false;
+									break;
+								}
+							}
+						}
+
+
+					}
+				}
+			}
+			catch (Exception e ) {
+				Console.WriteLine( "There was an error in the ClientSender : {0}", e.Message );
+			}
+		} 
+	} 
+}
