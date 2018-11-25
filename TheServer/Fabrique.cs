c403646badf0875f7481c8da697902cf5b25f379
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-25 21:01:21 +0100

    Added the Fabrique class

---
+++ TheServer/Fabrique.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using IRemote;
+
+namespace TheServer {
+	public class Fabrique : MarshalByRefObject, IFabrique {
+		public IMailBox getMailBox () {
+			return (IMailBox)new MailBox();
+		}
+	}
+}
