c0300d7aab35ba9eb18ff47d33e0f52d29a06716
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-25 20:36:17 +0100

    Revised project.

---
+++ IRemote/EmptyInterface.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace IRemote
+{
+	public interface EmptyInterface
+	{
+	}
+}
+
---
+++ IRemote/IFabrique.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace IRemote
+{
+	public interface IFabrique
+	{
+		IMailBox getMailBox();
+	}
+}
+
